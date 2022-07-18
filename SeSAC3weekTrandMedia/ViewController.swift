8f8ba0a136809eb8ff23d82d6d59eac880fcb3d9
rhksrb99 <rhksrb99@gmail.com>
2022-07-18 09:16:13 +0900

    Initial Commit

---
+++ SeSAC3weekTrandMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC3weekTrandMedia
+//
+//  Created by 박관규 on 2022/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
